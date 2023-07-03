5cbc8644e3531292d4b814ea99e1339ba4088368
Aaron Sequeira <96731649+aaronseq12@users.noreply.github.com>
2023-07-03 21:30:32 +0300

    Update test-models.py

--- littlelemon/tests/test-models.py
+++ littlelemon/tests/test-models.py
@@ -10,6 +10,4 @@ class MenuModelTest(TestCase):
         self.assertEqual(menu.name, 'Breakfast')
         self.assertEqual(menu.price, 20)
         self.assertEqual(menu.description, 'A selection of breakfast items')
-
-        # check that the calculate_total_cost method works correctly
         self.assertEqual(menu.calculate_total_cost(), 30)
